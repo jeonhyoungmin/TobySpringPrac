419df1caef60b5b228f4d43441f2914d6e1cd25a
jeonhyoungmin <jhmin7509@gmail.com>
2024-05-01 07:54:09 +0900

    project init

---
+++ src/main/java/com/surup/TobySpringPrac/user/dao/UserDao.java
@@ -0,0 +1,68 @@
+package com.surup.TobySpringPrac.user.dao;
+
+import com.surup.TobySpringPrac.user.domain.User;
+
+import java.sql.*;
+
+public class UserDao {
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/tobyspringprac", "toby", "pass123#");
+
+        PreparedStatement ps = c.prepareStatement(
+                "insert into users (id, name, password) values (?,?,?)"
+        );
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost/tobyspringprac", "toby", "pass123#"
+        );
+
+        PreparedStatement ps = c.prepareStatement(
+                "select * from users where id = ?"
+        );
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("surup");
+        user.setName("전형민");
+        user.setPassword("pass123#");
+
+        dao.add(user);
+
+        System.out.println("user.getId() = " + user.getId());
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId());
+    }
+}
