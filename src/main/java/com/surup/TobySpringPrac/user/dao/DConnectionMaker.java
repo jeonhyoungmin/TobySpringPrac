c76d47e779579f043e39436c0281b5808a3b142c
jeonhyoungmin <jhmin7509@gmail.com>
2024-05-09 23:59:04 +0900

    1.3.2 인터페이스의 도입
    "DB 커넥션을 제공하는 클래스에 대한 구체적인 정보는 모두 제거가 가능했지만, 초기에 한 번 어떤 클래스의 오브젝트를 사용할지를 결정하는 생성자의 코드는 제거되지 않고 남아 있다."

--- src/main/java/com/surup/TobySpringPrac/user/dao/DConnectionMaker.java
+++ src/main/java/com/surup/TobySpringPrac/user/dao/DConnectionMaker.java
@@ -4,8 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class SimpleConnectionMaker {
-    public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+public class DConnectionMaker implements ConnectionMaker {
+
+    @Override
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+        // D 사의 독자적인 방법으로 Connection을 생성하는 코드
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection c = DriverManager.getConnection("jdbc:mysql://localhost/tobyspringprac", "toby", "pass123#");
         return c;
