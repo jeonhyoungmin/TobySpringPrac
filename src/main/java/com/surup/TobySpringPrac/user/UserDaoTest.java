fc700989ae943f2057d8cff0d3abe2679c74069b
jeonhyoungmin <jhmin7509@gmail.com>
2024-05-12 08:18:26 +0900

    1.8.4 프로퍼티 값의 주입

--- src/main/java/com/surup/TobySpringPrac/user/UserDaoTest.java
+++ src/main/java/com/surup/TobySpringPrac/user/UserDaoTest.java
@@ -1,17 +1,17 @@
 package com.surup.TobySpringPrac.user;
 
-import com.surup.TobySpringPrac.user.dao.DaoFactory;
 import com.surup.TobySpringPrac.user.dao.UserDao;
 import com.surup.TobySpringPrac.user.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        // ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        // UserDao dao = context.getBean("userDao", UserDao.class);
-        UserDao dao = new DaoFactory().userDao();
+    public static void main(String[] args) throws SQLException {
+         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+         UserDao dao = context.getBean(UserDao.class);
 
         User user = new User();
         user.setId("surup");
