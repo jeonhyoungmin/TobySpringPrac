2698fd324314ab9e2e3f2903d16b447c57815ea3
jeonhyoungmin <jhmin7509@gmail.com>
2024-05-12 03:22:07 +0900

    1.5.1 오브젝트 팩토리를 이용한 스프링 IoC
    DaoFactory를 사용하는 애플리케이션 컨텍스트
    리플렉을 통한 접근은 보안 정책 강화로 허용되지 않음. 자바의 이전 버전과 라이브러리를 다운로드 받으면 될 거 같지만 시도해보지 않음.

--- src/main/java/com/surup/TobySpringPrac/user/UserDaoTest.java
+++ src/main/java/com/surup/TobySpringPrac/user/UserDaoTest.java
@@ -5,13 +5,16 @@ import com.surup.TobySpringPrac.user.dao.DConnectionMaker;
 import com.surup.TobySpringPrac.user.dao.DaoFactory;
 import com.surup.TobySpringPrac.user.dao.UserDao;
 import com.surup.TobySpringPrac.user.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("surup");
--- src/main/java/com/surup/TobySpringPrac/user/dao/DaoFactory.java
+++ src/main/java/com/surup/TobySpringPrac/user/dao/DaoFactory.java
@@ -1,19 +1,12 @@
 package com.surup.TobySpringPrac.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시
 public class DaoFactory {
 
-//    public UserDao userDao() {
-//        return new UserDao(new DConnectionMaker());
-//    }
-//
-//    public AccountDao accountDao() {
-//        return new AccountDao(new DConnectionMaker());
-//    }
-//
-//    public MessageDao messageDao() {
-//        return new MessageDao(new DConnectionMaker());
-//    }
-
+    @Bean // 오브젝트 생성을 담당하는 IoC용 메소드라는 표시
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
@@ -26,6 +19,7 @@ public class DaoFactory {
         return new MessageDao(connectionMaker());
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker(); // 분리해서 중복을 제거한 ConnectionMaker 타입 오브젝트 생성 코드
     }
