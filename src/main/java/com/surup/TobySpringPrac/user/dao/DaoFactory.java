d1b67d2b1c0b4df55bafd8cfbefc2e9ccc8291b6
jeonhyoungmin <jhmin7509@gmail.com>
2024-05-12 07:08:27 +0900

    1.7.5 메소드를 이용한 의존관계 주입

--- src/main/java/com/surup/TobySpringPrac/user/dao/DaoFactory.java
+++ src/main/java/com/surup/TobySpringPrac/user/dao/DaoFactory.java
@@ -8,7 +8,9 @@ public class DaoFactory {
 
     @Bean // 오브젝트 생성을 담당하는 IoC용 메소드라는 표시
     public UserDao userDao() {
-        return new UserDao(connectionMaker());
+        UserDao userDao = new UserDao();
+        userDao.setConnectionMaker(connectionMaker());
+        return userDao;
     }
 
     public AccountDao accountDao() {
